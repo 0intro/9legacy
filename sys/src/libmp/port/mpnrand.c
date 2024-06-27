11c43a1e45f2db1c9a4726ecab452cdbc774b97f
David du Colombier <0intro@gmail.com>
2024-06-27 20:36:08 +0200

    sys/src/libmp: add mpnrand function to generate uniform random number (thanks Cinap Lenrek)

---
+++ sys/src/libmp/port/mpnrand.c
@@ -0,0 +1,24 @@
+#include "os.h"
+#include <mp.h>
+#include <libsec.h>
+#include "dat.h"
+
+/* return uniform random [0..n-1] */
+mpint*
+mpnrand(mpint *n, void (*gen)(uchar*, int), mpint *b)
+{
+	int bits;
+
+	bits = mpsignif(n);
+	if(bits == 0)
+		abort();
+	if(b == nil){
+		b = mpnew(bits);
+		setmalloctag(b, getcallerpc(&n));
+	}
+	do {
+		mprand(bits, gen, b);
+	} while(mpmagcmp(b, n) >= 0);
+
+	return b;
+}
