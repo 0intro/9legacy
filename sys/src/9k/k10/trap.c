0040f9bbcb0444dee05a6876a95416d430b599bd
David du Colombier <0intro@gmail.com>
2026-06-26 21:51:38 +0000

    sys/src/9k/k10: guard sched() calls in trap handler (thanks Geoff Collyer)

--- sys/src/9k/k10/trap.c
+++ sys/src/9k/k10/trap.c
@@ -339,7 +339,7 @@ trap(Ureg* ureg)
 			if(ctl->irq == IdtPIC+IrqCLOCK || ctl->irq == IdtTIMER)
 				clockintr = 1;
 
-			if(up && !clockintr)
+			if(up && !clockintr && m->ilockdepth == 0)
 				preempted();
 		}
 	}
@@ -375,7 +375,7 @@ trap(Ureg* ureg)
 	splhi();
 
 	/* delaysched set because we held a lock or because our quantum ended */
-	if(up && up->delaysched && clockintr){
+	if(up && up->delaysched && clockintr && m->ilockdepth == 0){
 		sched();
 		splhi();
 	}
