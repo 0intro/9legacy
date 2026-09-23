73aca09068f5fd0d2caf6648a3ad8983e0ca279f
David du Colombier <0intro@gmail.com>
2026-09-23 21:50:29 +0000

    sys/src/libc: add casp for mips and mips64

--- sys/src/libc/mips64/atom.s
+++ sys/src/libc/mips64/atom.s
@@ -4,6 +4,8 @@
 
 #define	LL(base, rt)	WORD	$((060<<26)|((base)<<21)|((rt)<<16))
 #define	SC(base, rt)	WORD	$((070<<26)|((base)<<21)|((rt)<<16))
+#define	LLD(base, rt)	WORD	$((064<<26)|((base)<<21)|((rt)<<16))
+#define	SCD(base, rt)	WORD	$((074<<26)|((base)<<21)|((rt)<<16))
 #define	NOOP		WORD	$0x27
 
 TEXT ainc(SB), 1, $-8			/* long ainc(long *); */
@@ -48,5 +50,25 @@ spincas:
 	MOVW	$1, R1
 	RET
 fail:
+	MOVV	$0, R1
+	RET
+
+/*
+ * int casp(void** p, void* ov, void* nv);
+ * pointers are 64-bit, so compare and swap the whole doubleword
+ */
+TEXT casp(SB), 1, $-8
+	MOVV	ov+8(FP), R2
+	MOVV	nv+16(FP), R3
+spincasp:
+	LLD(1, 4)			/* R4 = *R1 */
+	NOOP
+	BNE	R2, R4, faspl
+	SCD(1, 3)			/* *R1 = R3 */
+	NOOP
+	BEQ	R3, spincasp		/* R3 == 0 means store failed */
+	MOVV	$1, R1
+	RET
+faspl:
 	MOVV	$0, R1
 	RET
