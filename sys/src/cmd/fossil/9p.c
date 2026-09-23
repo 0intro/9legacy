44bec1120a6ef9fda70a2aa53f72a4938fbe6819
David du Colombier <0intro@gmail.com>
2026-09-23 21:50:29 +0000

    sys/src/cmd/fossil: print fid numbers in hexadecimal in error messages (thanks Geoff Collyer)

--- sys/src/cmd/fossil/9p.c
+++ sys/src/cmd/fossil/9p.c
@@ -812,7 +812,7 @@ rTwalk(Msg* m)
 	if(t->fid != t->newfid){
 		nfid = fidGet(m->con, t->newfid, FidFWlock|FidFCreate);
 		if(nfid == nil){
-			werrstr("%s: walk: newfid 0x%ud in use",
+			werrstr("%s: walk: newfid 0x%ux in use",
 				argv0, t->newfid);
 			fidPut(ofid);
 			return 0;
